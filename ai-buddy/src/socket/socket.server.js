db754e72e136cb4678930882f3117840ad25cbc4
Kanak Yadav <yadavkanak109@gmail.com>
2026-04-07 16:36:25 +0530

    feat(socket): enhance socket setup with JWT token validation and cookie parsing

--- ai-buddy/src/socket/socket.server.js
+++ ai-buddy/src/socket/socket.server.js
@@ -1,11 +1,32 @@
 const socketIO = require('socket.io');
+const cookie = require('cookie');
+const jwt = require('jsonwebtoken');
 
-
-
-function setupSocket(server) {
+async function setupSocket(server) {
 
     const io = socketIO(server, {})
 
+
+    io.use((socket, next) => {
+
+        const cookies = socket.handshake.headers.cookie;
+        const token = cookie.parse(cookies || '').token;
+
+        if(!token) {
+            return next(new Error('Token is required!!!'));
+        }
+    
+    
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_TOKEN);
+    socket.user = decoded;
+    next();
+  }
+  catch (error) {
+    return next(new Error('Invalid token!!!'));
+  }
+
+    })
 
 
     io.on('connection', (socket) => {
@@ -17,4 +38,6 @@ function setupSocket(server) {
 
 
 
-module.exports = setupSocket;
+module.exports = {
+    setupSocket
+}
